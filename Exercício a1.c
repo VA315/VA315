5ecae1509ddabde77791abc8ac3531d0baf5e498
VA315 <valerialegria21@gmail.com>
2025-05-30 12:38:26 +0000

    modificação 1

---
+++ Exercício a1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(){
+    int idade = 25;
+    float altura = 1.75;
+    double saldobancario = 12345.67;
+    char inicial = "A";
+    char nome[20] = "Bruno";
+    printf("Idade: %d anos\n", idade);
+    printf("Altura: %.2f metros\n", altura);
+    printf("saldobancario:%2.f reais\n",saldobancario);
+    printf("Inicial do nome: %c\n",inicial);
+    printf("Nome: %s\n", nome);
+
+
+
+
+
+
+    return 0;
+}
